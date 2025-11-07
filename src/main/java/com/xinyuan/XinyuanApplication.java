ee227062b91c08a36aef0b18ad11337f55f7759d
ygb <517420999@qq.com>
2025-11-07 14:59:54 +0800

    初始化和基础内容

---
+++ src/main/java/com/xinyuan/XinyuanApplication.java
@@ -0,0 +1,13 @@
+package com.xinyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XinyuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XinyuanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xinyuan/XinyuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinyuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XinyuanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
