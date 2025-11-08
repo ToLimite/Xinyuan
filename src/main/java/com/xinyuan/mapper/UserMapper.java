84e8e582553e57dd434761c5d41d0a363b694f6e
ygb <517420999@qq.com>
2025-11-08 18:36:50 +0800

    删除一行未使用内容

--- src/main/java/com/xinyuan/mapper/UserMapper.java
+++ src/main/java/com/xinyuan/mapper/UserMapper.java
@@ -4,7 +4,6 @@ import com.xinyuan.pojo.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface UserMapper {
